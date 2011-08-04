e33dbcd5e8465c429e6c09fadab1b44c4ba68199
lite951 <pavelbakhilau@gmail.com>
2011-08-04 15:27:40 +0000

    fixed spacing

--- p38.py
+++ p38.py
@@ -14,8 +14,8 @@ def cp(i):
 def p38():
 	# lets do a dummy brute force
 	# we need the max value of the base integer
-	# max 9-digit pandigital number is 999999999 and smallest n is 2 so [i][2i] = 999999999
-	# since |[i]| <= |[2i]| only option is [i][2i] = [9999][99999] giving i = 9999
+	# max pandigital number is 999999999 and min n is 2 so [i][2i] = 999999999
+	# since |[i]| <= |[2i]| have [i][2i] = [9999][99999] giving i = 9999
 	return max(cp(i) for i in range(1, 9999))
 
 print(p38())
