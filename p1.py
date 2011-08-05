042dc3aa456df7febbfe7718bd1cf90e4ebaf583
Pavel Bakhilau <pavelbakhilau@gmail.com>
2011-08-04 23:16:42 -0400

    more solutions

--- p1.py
+++
@@ -1,8 +0,0 @@
-def smk(k, n): # sum multiples of k that are in [2, n)
-	nd = (n - 1) // k
-	return k * nd * (nd + 1) / 2
-
-def p1(n):
-	return smk(3, n) + smk(5, n) - smk(3 * 5, n)
-
-print p1(1000)
