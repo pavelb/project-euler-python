9440a1b078fa3e82e31749a46c3be3d0573435e7
Pavel Bakhilau <pavelbakhilau@gmail.com>
2011-08-05 01:09:59 -0400

    refactored p29

--- 29.py
+++ 29.py
@@ -1,8 +1,4 @@
 def main(a, b):
-	terms = set([])
-	for i in range(2, a + 1):
-		for j in range(2, b + 1):
-			terms.add(pow(i, j))
-	return len(terms)
+	return len(set(pow(i, j) for i in range(2, a + 1) for j in range(2, b + 1)))
 
 print(main(100, 100))
