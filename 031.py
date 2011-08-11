ecaee0a4c38a182a3aa06754bda8f111862dc736
Pavel Bakhilau <pavelbakhilau@gmail.com>
2011-08-11 19:07:33 -0400

    updated 31

--- 031.py
+++ 031.py
@@ -1,9 +1,8 @@
-def main(nums, target):
+def main(nums, target, i=0):
 	if target == 0:
 		return 1
-	if target < 0 or len(nums) == 0:
+	if target < 0 or i >= len(nums):
 		return 0
-	rest = nums[1:]
-	return sum(main(rest, target - n) for n in range(0, target + 1, nums[0]))
+	return sum(main(nums, target - n, i + 1) for n in range(0, target + 1, nums[i]))
 
 print(main([1, 2, 5, 10, 20, 50, 100, 200], 200))
