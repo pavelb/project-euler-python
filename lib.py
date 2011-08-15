d0670b299adcb4e56d1cab77411f5858b2650254
lite951 <pavelbakhilau@gmail.com>
2011-08-15 13:41:14 +0000

    home sync

--- lib.py
+++ lib.py
@@ -1,9 +1,10 @@
 from itertools import count, combinations, product
-from math import sqrt
+from math import sqrt, floor, log10
 
 class Primes:
 	computed = None
 	computedLen = 0
+	refLen = 0
 
 	def __init__(self):
 		self.compute(1024)
@@ -19,20 +20,15 @@ class Primes:
 				sieve[i * i: n + 1:i] = [False] * (n // i - i + 1)
 		self.computed = [i for i in range(n + 1) if sieve[i]]
 		self.computedLen = len(self.computed)
+		self.refLen = n
 
 	def get(self, i): # get the i-th prime, zero indexed
-		lim = self.computedLen
 		while i >= self.computedLen:
-			lim *= 2
-			self.compute(lim)
+			self.compute(2 * self.refLen)
 		return self.computed[i]
 
-	def gen(self, limit=float('inf')): # return a generator of primes <= limit
-		for i in count():
-			p = self.get(i)
-			if p > limit:
-				break
-			yield p
+	def gen(self):
+		return map(self.get, count())
 
 	def factors(self, n):
 		rootn = sqrt(n)
@@ -49,7 +45,7 @@ class Primes:
 		if n > 1:
 			yield n, 1
 
-	def divisors(self, n, proper=False):
+	def divisors(self, n, proper = False):
 		if n > 0:
 			parts = list([pow(b, i) for i in range(1, e + 1)] for b, e in self.factors(n))
 			for k in range(len(parts) + 1):
@@ -84,6 +80,40 @@ def num(digits):
 	for d in digits:
 		n = 10 * n + d
 	return n
+
+def numLen(n): # number if digits in n, equivalent to len(digits(n))
+	return 1 + floor(log10(n)) if n > 0 else 1
+
+def fibonacci():
+	a, b = 0, 1
+	while True:
+		yield a
+		a, b = b, a + b
+
+def pythagoreanTriples(a = 3, b = 4, c = 5):
+	# return a generator for the triplets in order of increasing perimiter
+	# en.wikipedia.org/wiki/Pythagorean_triple#Parent.2Fchild_relationships
+	yield a, b, c
+	gens = [
+		((k * a, k * b, k * c) for k in count(2)),
+		pythagoreanTriples(a - 2 * b + 2 * c, 2 * a - b + 2 * c, 2 * a - 2 * b + 3 * c),
+		pythagoreanTriples(a + 2 * b + 2 * c, 2 * a + b + 2 * c, 2 * a + 2 * b + 3 * c),
+		pythagoreanTriples(-a + 2 * b + 2 * c, -2 * a + b + 2 * c, -2 * a + 2 * b + 3 * c)
+	]
+	opts = [(sum(v), v, i) for i, v in enumerate(map(next, gens))]
+	while True:
+		_, tuple, i = min(opts)
+		yield tuple
+		tuple = next(gens[i])
+		opts[i] = (sum(tuple), tuple, i)
+
+def pandigital(n, k = 9):
+	saw = [False] * k
+	for d in digits(n):
+		if d == 0 or d > k or saw[d - 1]:
+			return False
+		saw[d - 1] = True
+	return all(saw)
 
 def charInd(char):
 	return ord(char) - 64
