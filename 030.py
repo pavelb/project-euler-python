be4a16a97fdb347fc0f76e8d1d5d9f0d1a806889
Pavel Bakhilau <pavelbakhilau@gmail.com>
2013-05-31 01:45:21 -0700

    speed-up 30

--- 030.py
+++ 030.py
@@ -1,9 +1,16 @@
-from lib import digits
 from itertools import count
+
+def good(n, exp):
+	on = n
+	m = 0
+	while n > 0 and m <= on:
+		m += pow(n % 10, exp)
+		n //= 10
+	return m == on
 
 def main(exp):
 	limit = next(n for n in count(1) if pow(10, n) > pow(9, exp) * n)
-	return sum(n for n in range(2, pow(10, limit)) if n == sum(pow(d, exp) for d in digits(n)))
+	return sum(n for n in range(2, pow(10, limit)) if good(n, exp))
 
 if __name__ == '__main__':
 	print(main(5)) # 443839
