22a6cad9e8bde478bad75dc69e68e4360445dcfe
Pavel Bakhilau <pavelbakhilau@gmail.com>
2013-05-31 02:12:31 -0700

    speed-up 31 and add version using dp

--- 031.py
+++ 031.py
@@ -1,9 +1,14 @@
-def main(nums, target, i = 0):
-	if target == 0:
-		return 1
-	if i >= len(nums):
-		return 0
-	return sum(main(nums, target - n, i + 1) for n in range(0, target + 1, nums[i]))
+def main(nums, target):
+	mem = [[0] * len(nums) for _ in range(target + 1)]
+	for n in range(target + 1):
+		if n % nums[-1] == 0:
+			mem[n][-1] = 1
+
+	for i in range(len(nums) - 2, -1, -1):
+		for n in range(target + 1):
+			mem[n][i] = sum(mem[n - m][i + 1] for m in range(0, n + 1, nums[i]))
+
+	return mem[target][0]
 
 if __name__ == '__main__':
 	print(main([1, 2, 5, 10, 20, 50, 100, 200], 200)) # 73682
---
+++ 031_0.py
@@ -0,0 +1,19 @@
+def memoized(fn):
+	mem = {}
+	def rv(nums, target, i = 0):
+		key = (target, i)
+		if key not in mem:
+			mem[key] = fn(nums, target, i)
+		return mem[key]
+	return rv
+
+@memoized
+def main(nums, target, i = 0):
+	if target == 0:
+		return 1
+	if i >= len(nums):
+		return 0
+	return sum(main(nums, target - n, i + 1) for n in range(0, target + 1, nums[i]))
+
+if __name__ == '__main__':
+	print(main([1, 2, 5, 10, 20, 50, 100, 200], 200)) # 73682
