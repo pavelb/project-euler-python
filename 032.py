4afaa1e143a71828dadb0621b9e2d75c2b8d885f
Pavel Bakhilau <pavelbakhilau@gmail.com>
2013-06-07 22:29:14 -0700

    speed-up 32

--- 032.py
+++ 032.py
@@ -1,18 +1,26 @@
 from lib import num
-from itertools import permutations
+from itertools import permutations, combinations, chain, product
+
+def subsets(nums, sizes):
+	for a in chain.from_iterable(combinations(nums, r) for r in sizes):
+		a = set(a)
+		yield a, nums - a
+
+
+def genNums(digits):
+	return map(num, permutations(digits))
+
 
 def main(length):
-	# generate all pandigital numbers and try inserting * and = everywhere
-	def seq(length):
-		nums = set(range(1, length + 1))
-		for ansLen in range(1, length - 1):
-			for ansDigits in permutations(nums, ansLen):
-				ans = num(ansDigits)
-				dgen = permutations(nums - set(ansDigits))
-				igen = range(1, length - ansLen - 1)
-				if any(num(d[:i]) * num(d[i:]) == ans for d in dgen for i in igen):
-					yield ans
-	return sum(seq(length))
+	good = set()
+	nums = set(range(1, length + 1))
+	for restDigits, answerDigits in subsets(nums, range(2, len(nums))):
+		answers = set(genNums(answerDigits))
+		for leftDigits, rightDigits in subsets(restDigits, range(1, len(restDigits) // 2 + 1)):
+			for a, b in product(genNums(leftDigits), genNums(rightDigits)):
+				if a * b in answers:
+					good.add(a * b)
+	return sum(good)
 
 if __name__ == '__main__':
 	print(main(9)) # 45228
