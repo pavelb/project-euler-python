f693bcce3359bbe1e425a9c7f5c946f382b0a840
Pavel Bakhilau <pavelbakhilau@gmail.com>
2013-06-08 01:22:46 -0700

    speed-up 30

--- 030.py
+++ 030.py
@@ -1,16 +1,20 @@
-from itertools import count
-
-def good(n, exp):
-	on = n
-	m = 0
-	while n > 0 and m <= on:
-		m += pow(n % 10, exp)
-		n //= 10
-	return m == on
+from itertools import count, product
+from lib import num
 
 def main(exp):
-	limit = next(n for n in count(1) if pow(10, n) > pow(9, exp) * n)
-	return sum(n for n in range(2, pow(10, limit)) if good(n, exp))
+	rv = 0
+	for r in count(2):
+		for d in product(range(10), repeat=r):
+			if d[0] == 0:
+				continue
+			n = num(d)
+			if (n - sum(d)) % exp: # assume exp is prime and use fermats theorem
+				continue
+			if n == sum(b ** exp for b in d):
+				rv += n
+		if 10 ** r > 9 ** exp * r: # if max rhs smaller than min lhs
+			break
+	return rv
 
 if __name__ == '__main__':
 	print(main(5)) # 443839
