0f996fb9ced9a3fbecd61d4ecf066a86b1601ae4
Pavel Bakhilau <pavelbakhilau@gmail.com>
2013-06-08 02:36:51 -0700

    speed-up 43

--- 043.py
+++ 043.py
@@ -1,11 +1,24 @@
 from lib import num
-from itertools import permutations
+
+def gen(nums, primes, pos=0, prefix=0):
+	if pos >= 4 and prefix % 10 ** 3 % primes[pos - 4] != 0:
+		return
+
+	if not nums:
+		yield prefix
+		return
+
+	for n in nums:
+		nums.remove(n)
+		for g in gen(nums, primes, pos + 1, 10 * prefix + n):
+			yield g
+		nums.add(n)
+
 
 def main():
+	# trick: generate all permutations of 0..9 by extending valid prefixes
 	primes = (2, 3, 5, 7, 11, 13, 17)
-	good = lambda k: all(num(k[i + 1:i + 4]) % p == 0 for i, p in enumerate(primes))
-	pandigitals = permutations(range(10))
-	return sum(map(num, filter(good, pandigitals)))
+	return sum(gen(set(range(10)), primes))
 
 if __name__ == '__main__':
 	print(main()) # 16695334890
