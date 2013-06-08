f665d24a6f49cd4e1571ddd97f22068343c1becc
Pavel Bakhilau <pavelbakhilau@gmail.com>
2013-06-08 01:48:08 -0700

    speed-up 35

--- 035.py
+++ 035.py
@@ -1,13 +1,13 @@
 from lib import Primes, digits, num
 from itertools import takewhile
 
-primes = Primes()
-
-def circular(p):
-	d = digits(p)
-	return all(primes.isPrime(num(d[i:] + d[:i])) for i in range(1, len(d)))
-
 def main(limit):
+	primes = Primes(limit)
+
+	def circular(p):
+		d = digits(p)
+		return all(primes.isPrime(num(d[i:] + d[:i])) for i in range(1, len(d)))
+
 	return sum(map(circular, takewhile(lambda n: n < limit, primes.gen())))
 
 if __name__ == '__main__':
