6a7e89612424d9f53b2486b269bf2d8e19e8b5b2
Pavel Bakhilau <pavelbakhilau@gmail.com>
2013-06-09 03:00:59 -0700

    speed-up 52

--- 052.py
+++ 052.py
@@ -2,9 +2,12 @@ from lib import digitCountMap, same
 from itertools import count
 
 def main(limit):
-	facs = range(1, limit + 1)
+	# trick: want the digits of 2x, 3x, ... to equal
+	# if limit > 2 we must have 2x = 3x mod 9 or x = 0 mod 9
+	skip = 1 if limit < 3 else 9
+	facs = range(2, limit + 1)
 	good = lambda n: n > 0 and same(map(digitCountMap, (f * n for f in facs)))
-	return next(filter(good, count()))
+	return next(filter(good, count(0, skip)))
 
 if __name__ == '__main__':
 	print(main(6)) # 142857
