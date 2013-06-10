0f21b60e9a9b07e366817a59ed72e207235a425e
Pavel Bakhilau <pavelbakhilau@gmail.com>
2013-06-09 21:52:35 -0700

    speed-up

--- 069.py
+++ 069.py
@@ -3,7 +3,12 @@ from lib import Primes
 primes = Primes()
 
 def main(lim):
-	return max(range(1, lim), key=lambda n: n / primes.phi(n))
+	# trick: minimize phi(n)
+	prod = 1
+	for p in primes.gen():
+		prod *= p
+		if prod * p > lim:
+			return prod
 
 if __name__ == '__main__':
 	print(main(1000000)) # 510510
