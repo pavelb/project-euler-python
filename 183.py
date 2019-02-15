8241b741d5db26167ee31cc1b5ea05cf2eaebdb8
pavelb <pavelbakhilau@gmail.com>
2019-02-15 15:40:28 -0800

    Update 183.py

--- 183.py
+++ 183.py
@@ -2,8 +2,12 @@ from lib import gcd
 import math
 
 def D(N):
+	# d/dk((N/k)^k) = 0 -> k=N/2
 	k = int(N / math.e + 0.5)
+	# Check if N/k is a terminating decimal
+	# Reduce by gcd
 	k //= gcd(N, k)
+	# Reduce by terminating decimal divisors
 	while k % 2 == 0: k //= 2
 	while k % 5 == 0: k //= 5
 	return N if k > 1 else -N
