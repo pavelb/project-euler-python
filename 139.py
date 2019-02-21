52bbf2787848ba69435446b339948b9e62d4f9b9
pavelb <pavelbakhilau@gmail.com>
2019-02-21 11:34:04 -0800

    Update 139.py

--- 139.py
+++ 139.py
@@ -1,11 +1,32 @@
-# unfinished
-from lib import pythagoreanTriples
+# Consider a non-reducible pythagorean triple (a, b, c)
+# We want c*c-2*a*b | c*c
+# But c*c = a*a+b*b = (a-b)**2+2*a*b
+# Therefore c*c-2*a*b = (b-a)**2
+# Therefore we want (b-a) | c
+# But c*c = a*a + b*b
+# If k is a factor of c it must be a factor of a and b too
+# But (a, b, c) is not reducible
+# Therefore b-a=1
+# Therefore a*a+(a+1)**2=c*c
+# Therefore a = ((2*c*c-1)**.5-1)/2
+# Therefore 2*c*c-1 must be some square d*d where a = (d-1)/2
+# Therefore d*d-2*c*c=-1
+# This is a negative Pell's equation for n=2
+# Find integer solutions (d, c) to find the corresponding triangles (a, b, c)
+# The triangles are reduced so multiply by integers to reach target perimeter
 
-# (b-a) | c
-# a*a + b*b = k*k*(a-b)**2
-# (a-b)**2+2*a*b = k*k*(a-b)**2
-# 2*a*b = (k*k*-1)*(a-b)**2
-for a, b, c in pythagoreanTriples():
-    if a + b + c >= 1e8:
-        break
-    print(a+b+c, a, b, c)
+def main(lim):
+  d, c = 1, 1
+  rv = 0
+  while True:
+    d, c = 3*d+4*c, 2*d+3*c
+    a = (d-1)//2
+    b = a+1
+    m = lim // (a+b+c)
+    if m == 0:
+      break
+    rv += m
+  return rv
+
+if __name__ == '__main__':
+  print(main(100000000))
