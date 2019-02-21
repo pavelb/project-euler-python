8384a9f058df8e053c49227ece5d88001e6501fd
pavelb <pavelbakhilau@gmail.com>
2019-02-21 12:33:35 -0800

    Fix reasoning

--- 139.py
+++ 139.py
@@ -1,27 +1,25 @@
-# Consider a non-reducible pythagorean triple (a, b, c)
+# Consider (a pythagorean triple (a, b, c)
 # We want c*c-2*a*b | c*c
 # But c*c = a*a+b*b = (a-b)**2+2*a*b
 # Therefore c*c-2*a*b = (b-a)**2
-# Therefore we want (b-a) | c
-# But c*c = a*a + b*b
-# If k is a factor of c it must be a factor of a and b too
-# But (a, b, c) is not reducible
-# Therefore b-a=1
-# Therefore a*a+(a+1)**2=c*c
-# Therefore a = ((2*c*c-1)**.5-1)/2
-# Therefore 2*c*c-1 must be some square d*d where a = (d-1)/2
-# Therefore d*d-2*c*c=-1
+# We want (b-a) | c
+# Let k=b-a so that c = m*k
+# Therefore a*a+(a+k)**2=m*m*k*k
+# Solving for a gives a = k*((2*m*m-1)**.5-1)/2
+# 2*m*m-1 must be some square d*d making a = k*(d-1)/2
+# This gives equation d*d-2*m*m=-1
 # This is a negative Pell's equation for n=2
-# Find integer solutions (d, c) to find the corresponding triangles (a, b, c)
-# Note a+b+c = (d-1)/2+(d-1)/2+1+c = d+c
-# The triangles are reduced so multiply perimeter by integers to reach target
+# Find integer solutions (d, m) to find the corresponding triangles (a, b, c)
+# Note a+b+c = k*(d-1)/2 + k*(d-1)/2 + k + k*m = k*(d+m)
+# Use unique solutions (d, m) then multiply by all integers k to reach target
+# Or simply count how many such integers exist by dividing lim by (d+m)
 
 def main(lim):
-  d, c = 1, 1
+  d, m = 1, 1
   rv = 0
-  while d+c < lim:
-    d, c = 3*d+4*c, 2*d+3*c
-    rv += lim // (d+c)
+  while d+m < lim:
+    d, m = 3*d+4*m, 2*d+3*m
+    rv += lim // (d+m)
   return rv
 
 if __name__ == '__main__':
