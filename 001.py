035ccbc383bb858121338a91ee3c84ad342e419b
pavelb <pavelbakhilau@gmail.com>
2019-09-17 18:18:42 -0700

    Update 001.py

--- 001.py
+++ 001.py
@@ -2,8 +2,8 @@ def smk(k, n): # sum of multiples of k in [2, n)
 	nd = (n - 1) // k
 	return k * nd * (nd + 1) // 2
 
-def main(primeCount):
-	return smk(3, primeCount) + smk(5, primeCount) - smk(3 * 5, primeCount)
+def main(n):
+	return smk(3, n) + smk(5, n) - smk(3 * 5, n)
 
 if __name__ == '__main__':
 	print(main(1000)) # 233168
